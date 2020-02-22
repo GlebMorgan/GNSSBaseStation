604ce72c7b68798414725b9741306db7804f85aa
Gleb <glebmorgan@gmail.com>
2020-02-22 16:15:03 +0300

    build(mvbs.py): create tmpfs directory on startup
    Add action to set mvbs_tmpfiles.d.conf as systemd-tmpfiles config file
    Provide output showing changes to system config in autorun_mvbs.sh

--- app/autorun_mvbs.sh
+++ app/autorun_mvbs.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
+# add configuration file to systemd-tmpfiles to create tmpfs directory for temporary data
+sudo mv ~/app/mvbs_tmpfiles.d.conf /usr/lib/tmpfiles.d/mvbs.conf
+
 # Copy current crontab
 crontab -l > crontab_temp.txt
 
 # Echo new cron into crontab file
 echo "* * * * * >/dev/null 2>&1
-@reboot sudo mkdir /run/user/test && sudo chown pi: /run/user/test && sudo chmod 775 /run/user/test/
 @reboot python /home/pi/app/mvbs.py start -a > /home/pi/app/mvbs.log" \
 >> crontab_temp.txt
 
@@ -14,3 +16,12 @@ crontab crontab_temp.txt
 
 # CleanUp
 rm crontab_temp.txt
+
+echo -e "\ncrontab:"
+crontab -l
+
+echo -e "\ntmpfiles.d:"
+cat /usr/lib/tmpfiles.d/mvbs.conf
+
+echo -e "\n/run/user:"
+ls -l /run/user
