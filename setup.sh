23377fc0823a3f71f9ca564c88a085f67eb1be78
Gleb <glebmorgan@gmail.com>
2020-02-22 22:48:25 +0300

    build(.gitignore): ignore all test*.txt files

--- setup.sh
+++ setup.sh
@@ -24,8 +24,8 @@ echo "
 export LS_OPTIONS='--color=auto'
 eval \"\`dircolors\`\"
 alias ls='ls $LS_OPTIONS'
-alias ll='ls $LS_OPTIONS -l'
-alias l='ls $LS_OPTIONS -lA'
+alias l='ls $LS_OPTIONS -l'
+alias ll='ls $LS_OPTIONS -lA'
 " >> ~/.profile
 
 #########################################################################
