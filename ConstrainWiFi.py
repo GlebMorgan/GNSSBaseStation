2f2c32eb1202ed80c1dde5a6edd314ac6cf27e94
GlebMorgan <glebmorgan@gmail.com>
2020-02-24 18:24:12 +0300

    build: add script allowing to connect via WiFi to odincova.ddns.net only
    Resolves domain name to IP
    Modifies Windows routing table to forward all internet traffic (0.0.0.0)
    via Ethernet only, and odincova.ddns.net-addressed traffic via WiFi

---
+++ ConstrainWiFi.py
@@ -0,0 +1,31 @@
+from socket import gethostbyname, gaierror
+from subprocess import check_call
+
+
+HOST = 'odincova.ddns.net'
+
+
+try:
+
+    IP = gethostbyname(HOST)
+
+    commands = (
+        'route DELETE 0.0.0.0',
+        f'route DELETE {IP}',
+        'route ADD 0.0.0.0 MASK 0.0.0.0 192.168.116.115 IF 13',
+        f'route ADD {IP} MASK 255.255.255.255 192.168.43.1 IF 15',
+        'route PRINT'
+    )
+
+    for command in commands:
+        check_call(command, shell=True)
+
+    input("\nDONE")
+    exit(0)
+
+except Exception as e:
+    if type(e) is gaierror:
+        print(f"Error: failed to resolve '{HOST}'")
+    print(e)
+    input("Script will now exit")
+    exit(1)
