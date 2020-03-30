304002a559d3b3a921b4f4c96dbe3a2355b9444b
Gleb <glebmorgan@gmail.com>
2020-03-30 20:23:24 +0300

    Stop mvbs before forwarding uCenter traffic

--- ucenter_forward.sh
+++ ucenter_forward.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
+echo "Interrupting mvbs process..."
+python ~/app/mvbs.py stop
+
+echo "Forwarding /dev/serial0 to uCenter on port 2020..."
 sudo socat tcp-listen:2020,reuseaddr /dev/serial0,b115200,raw,echo=0
+
+echo "Disconnected"
