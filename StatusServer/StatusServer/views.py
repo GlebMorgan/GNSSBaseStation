cbec3e919fc55e6cc9232b6032d602e1922ae22e
Gleb <glebmorgan@gmail.com>
2020-04-22 15:24:26 +0300

    feat(StatusServer): auto update status web page
    Add 'update_rate' context variable when rendering table.html template
    Add <meta http-equiv="refresh"> tag to table.html
    Refactor: import config file from views.py and pass it to get_status()
    Add 'status_update_rate' parameter to config.toml

--- StatusServer/StatusServer/views.py
+++ StatusServer/StatusServer/views.py
@@ -2,13 +2,22 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from . import controller
 
+import toml
+
+
+CONFIG_FILE = '/home/pi/app/config.toml'
+
+config = toml.load(str(CONFIG_FILE))
+
 
 def status_table(request):
     context = {
+        # Dirty fix... Empty string is invalid parameter and will be ignored
+        'update_rate': config['status_update_rate'] or '',
         'table_title': 'Base station status',
         'data': [
             dict(name=name, value=parameter)
-            for name, parameter in controller.get_status().items()
+            for name, parameter in controller.get_status(config).items()
         ],
     }
     return render(request, 'table.html', context)
