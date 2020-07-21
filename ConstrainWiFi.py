de290871f7bf8d687a59e5b205ea809591be5046
GlebMorgan <glebmorgan@gmail.com>
2020-07-21 11:51:06 +0300

    feat: redesign ConstrainWiFi.py to accept hosts from file and sys.argv

--- ConstrainWiFi.py
+++ ConstrainWiFi.py
@@ -1,28 +1,43 @@
 from socket import gethostbyname, gaierror
 from subprocess import check_call
+from sys import argv
+from pathlib import Path
 
+print(repr(argv))
+HOSTS_FILE = Path('Constrained hosts.txt')
 
-HOST = 'odincova.ddns.net'
+if argv[1:]:
+    HOSTS = argv[1:]
+elif HOSTS_FILE.exists():
+    HOSTS = [item.strip() for item in HOSTS_FILE.read_text().split('\n')]
+else:
+    raise RuntimeError("Hosts are not provided neither in args nor in 'Constrained hosts' file")
 
 
 try:
 
-    IP = gethostbyname(HOST)
+    IPs = [gethostbyname(host) for host in HOSTS]
+
+    print("Constraining targets:")
+    for host, ip in zip(HOSTS, IPs):
+        print(f"    Route {host}({ip}) via 192.168.43.1")
+    print()
 
     commands = (
         'route DELETE 0.0.0.0',
-        f'route DELETE {IP}',
+        *(f'route DELETE {ip}' for ip in IPs),
         'route ADD 0.0.0.0 MASK 0.0.0.0 192.168.116.115 IF 13',
-        f'route ADD {IP} MASK 255.255.255.255 192.168.43.1 IF 15',
+        *(f'route ADD {ip} MASK 255.255.255.255 192.168.43.1 IF 15' for ip in IPs),
         'route PRINT'
     )
 
     for command in commands:
+        print(f"Command: {command}")
         check_call(command, shell=True)
+        print()
 
     print("\nDONE")
     check_call('pause >nul', shell=True)
-    exit(0)
 
 except Exception as e:
     if type(e) is gaierror:
